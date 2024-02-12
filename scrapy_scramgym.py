a3934e54bd8fc5b34757b3589aa4a9862a9a47f3
sebastian alegria sandoval <tatan.alegria@gmail.com>
2024-02-12 19:44:32 -0300

    se hace scrap de scramgym

--- scrapy_scramgym.py
+++ scrapy_scramgym.py
@@ -1,64 +1,32 @@
-import random
-from time import sleep
-from selenium.webdriver.common.by import By
-from selenium import webdriver # pip install selenium
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import requests
 import pandas as pd
-import csv
 import os
-# Asi podemos setear el user-agent en selenium
-chromedriver = "C:/Users/tatan/OneDrive/Documentos/proyecto-webscraping/chromedriver.exe"
 
-option = webdriver.ChromeOptions()
+headers = {
+    # El encabezado de referer es importante. Sin esto, este API en especifico me respondera 403
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+}
 
-option.binary_location = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"
+response = requests.get('https://scramgym.gymmasteronline.com/portal/getcompanymemberships?session=eyJjb250ZXh0X2NvbXBhbnlpZCI6MiwibGFuZ3VhZ2UiOiJlbl9HQiIsImlwX2FkZHIiOiI3Ny43MS4yNTAuMjAifQ.Zcaqyg.Af4XZObgwacDCAU4qYQJaxsSO4k&companyid=2&divisionids=%5B%5D&renew_ref=&discount_list=&discount_code=&company_group=None', headers=headers)
+# print(response)
 
-s = Service(chromedriver)
+# Parseo la respuesta en formato JSON. Requests automaticamente lo convierte en un diccionario de Python
+data = response.json()
+# print(data)
 
-driver = webdriver.Chrome(service=s, options=option)
-# Voy a la pagina que quiero
-driver.get('https://scramgym.com/member-portal/gym-registration/')
-sleep(3)
-# driver.refresh() # Solucion de un bug extraño en Windows en donde los anuncios solo cargan al hacerle refresh a la página
-# sleep(2) # Esperamos que cargue el boton
-driver.execute_script("""window.scrollTo(0, 1000)""")
-sleep(5)
-# Busco el boton para cargar mas informacion
-boton_div = WebDriverWait(driver,5).until(
-    EC.presence_of_element_located((By.XPATH, "//div[contains(@class,'signup-box')]//a[@data-divisionid='15']"))
-)
-# boton_div = driver.find_element(By.XPATH, "//div[contains(@class,'col-xs-12')][3]")
-boton_div.click()
+gyms_totales = []
 
-divs_gym = driver.find_elements(By.XPATH, "//a[contains(@class,'btn-addmembership')]")
-# precios_gym= driver.find_element(By.XPATH, "//div[@class='tile-item-price']//span[@class='tile-item-price-value']")
-# periodos_gym= driver.find_element(By.XPATH, "//div[@class='tile-item-price']//span[@class='tile-item-price-interval']")
-# divs_gym = driver.find_elements(By.XPATH, "//div[contains(@class,'row')]")
-# print(str(len(divs_gym)))
-contador = 0
-templist = []
-for div in divs_gym:
-    # print("DIV--------------------"+str(contador))
-    # print(div.get_attribute('innerHTML'))
-    # print("DIV--------------------"+str(contador))
-    precio_gym = div.find_element('xpath', "./span[contains(@class,'membership-price')]/h2").text
-    periodo_gym = div.find_element('xpath', "./span[contains(@class,'membership-details')]/h4").text
-    print(precio_gym+"/"+periodo_gym)
-    Table_dict = {
-            'periodo': periodo_gym,
-            'precio': precio_gym,
+gyms = data["memberships"]
+for gym in gyms:
+    if gym['divisionid'] == 15:
+        gyms_totales.append ({
+            'periodo':  gym['name'],
+            "precio": gym['price'],
             'moneda': 'Euro',
             'gym': 'scramgym'
-        }
-    templist.append(Table_dict)
-    df = pd.DataFrame(templist)
-    contador+=1
-
-# Aquí haré que me lo deje en un archivo csv
+        })
+df = pd.DataFrame(gyms_totales)
+# print (df)
 ruta_del_archivo = 'output.csv'
 if os.path.exists(ruta_del_archivo):
   print("El archivo ya existe, agregando resultados al final")
@@ -66,3 +34,4 @@ if os.path.exists(ruta_del_archivo):
 else:
   print("El archivo no existe, creando archivo")
   df.to_csv('output.csv', mode='w', index=False)
+# si el archivo existe, hacer con appen, si no existe con w
